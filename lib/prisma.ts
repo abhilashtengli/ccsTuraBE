4e0573c30bbfa0413738b0180dc2d79d5d2327aa
Abhilash <abhilasht0007.t@gmail.com>
2025-04-11 12:13:13 +0530

    Prisma update

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,5 @@
-import { PrismaClient } from "generated/prisma";
+import { PrismaClient } from "@prisma/client";
+
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
@@ -7,7 +8,7 @@ const globalForPrisma = globalThis as unknown as {
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: ['query'],
+    log: ['query'], // optional: useful for debugging
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
